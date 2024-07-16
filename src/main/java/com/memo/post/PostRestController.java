52fabf2fea4fbe0a17b2a6fd7f6d3e77eecc12a4
DESKTOP-92INLTK\user <KimSungJun-01>
2024-07-16 18:50:58 +0900

    24.07.16

---
+++ src/main/java/com/memo/post/PostRestController.java
@@ -0,0 +1,11 @@
+package com.memo.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.memo.post.bo.PostBO;
+
+@RestController
+public class PostRestController {
+	
+}
--- src/main/java/com/memo/user/UserController.java
+++ src/main/java/com/memo/user/UserController.java
@@ -4,13 +4,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/user")
 @Controller
 public class UserController {
-	
+
+	/**
+	 * 회원가입 화면
+	 * @return
+	 */
 	@GetMapping("/sign-up-view")
 	public String signUpView() {
 		// 가운데 레이아웃 조각만 내려주면 전체 레이아웃으로 구성된다.
 		return "user/signUp";
+	}
+	
+	/**
+	 * 로그인 화면
+	 * @return
+	 */
+	@GetMapping("/sign-in-view")
+	public String signInView() {
+		return "user/signIn";
+	}
+	
+	/**
+	 * 로그아웃 API
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/sign-out")
+	public String signOut(HttpSession session) {
+		// session 내용 비움
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		session.removeAttribute("userName");
+		
+		// 로그인 페이지로 이동
+		return "redirect:/user/sign-in-view";
 	}
 }
